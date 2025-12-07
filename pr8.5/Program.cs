bff13d0eb73a23ebc7f7d3c71bccfa3bf003ab03
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-12-07 23:43:29 +0300

    сделал

--- pr8.5/Program.cs
+++ pr8.5/Program.cs
@@ -27,6 +27,40 @@ namespace pr8._5
                 }
                 Console.WriteLine();
             }
+            int kol = 0;
+            for (int i = 0; i < stroka; i++)
+            {
+                int min = mas[i, 0];
+                int mini = 0;
+                for (int j = 0; j < stolbec; j++)
+                {
+                    if (mas[i, j] < min)
+                    {
+                        min = mas[i, j];
+                        mini = j;
+                    }
+                }
+                int max = mas[0, mini];
+                int maxi = 0;
+                for (int k = 0; k < stroka; k++)
+                {
+                    if (mas[k, mini] > max)
+                    {
+                        max = mas[k, mini];
+                        maxi = k;
+                    }
+                }
+                int sedl = min;
+                if (max == min)
+                {
+                    kol++;
+                    Console.WriteLine($"{kol}. [{maxi + 1},{mini + 1}] = {sedl}");
+                }
+            }
+            if (kol == 0)
+            {
+                Console.WriteLine($"Седловых точек нет");
+            }
         }
     }
 }
